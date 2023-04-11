587e3c85b116adffea27f76d8f7cc69f467f5c53
Murbanski58 <114615052+Murbanski58@users.noreply.github.com>
2023-04-11 16:00:38 -0500

    pytest

--- starter_test.py
+++ starter_test.py
@@ -20,53 +20,44 @@ from oop_loan_pmt import *
 ### unit tests ###
 def getDiscountFactor():
     """
-    GIVEN a user enters the year they were born
-    WHEN that year is passed to this function
-    THEN the user's age is accurately calculated
+    GIVEN a user enters loan information
+    WHEN the calculateDiscountFactor is called
+    THEN the loan's discount is calculated
     """
-    print("\r")  # carriage return
-    print(" -- calculate_current_age unit test")
-    assert (
-        calculate_current_age(2000) == 23
-    )  # STATIC: will change as the years progress
+    loan = Loan(loanAmount = 100000, numberYears = 30, annualRate = .06)
+    loan.calculateDiscountFactor()
+    print("\r")  
+    print(" -- calculateDiscountFactor unit test")
+    assert loan.getDiscountFactor() == pytest.approx(
+        166.79, rel = 1e-3
+    )  
+
+def LoanPayment():
+    """
+    GIVEN a user enters their loan details
+    WHEN calculateLoanPmt method is called
+    THEN the loan payment is calculated
+    """
+    loan = Loan(loanAmount=100000, numberYears=30, annualRate=0.06)
+    loan.calculateLoanPmt()
+    print("\r")
+    print(" -- calculateLoanPmt method unit test")
+    assert loan.getLoanPmt() == pytest.approx(
+        599.55, rel=1e-3
+    ) 
 
 
-def calculateDiscountFactor():
+#Functional Test
+def getcalculateLoanPmt():
     """
-    GIVEN a user enters the year they were born
-    WHEN that year is passed to this function
-    THEN the user's age is accurately calculated
+    GIVEN a user enters loan details
+    WHEN user clicks calculate
+    THEN the the monthly payment is calculated
     """
-    birth_year = 1995
-    today = date.today()
-    expected_age = today.year - birth_year
-    assert (
-        calculate_current_age(birth_year) == expected_age
-    )  # DYNAMIC: calculates the current year
-
-def calculateLoanPmt():
-    """
-    GIVEN a user enters the year they were born
-    WHEN that year is passed to this function
-    THEN the user's age is accurately calculated
-    """
-    birth_year = 1995
-    today = date.today()
-    expected_age = today.year - birth_year
-    assert (
-        calculate_current_age(birth_year) == expected_age
-    )  # DYNAMIC: calculates the current year
-
-
-def getLoanPmt():
-    """
-    GIVEN a user enters the year they were born
-    WHEN that year is passed to this function
-    THEN the user's age is accurately calculated
-    """
-    birth_year = 1995
-    today = date.today()
-    expected_age = today.year - birth_year
-    assert (
-        calculate_current_age(birth_year) == expected_age
-    )  # DYNAMIC: calculates the current year
+    response = client.post(
+        "/", data={"loanAmt": "100000", "lengthOfLoan": "30", "intRate": "0.06"}
+    )
+    print("\r")
+    print(" -- calculate loan functional test")
+    assert response.status_code == 200
+    assert b"$599.55" in response.data
